585553e426b804dcf8029b1ac45bf4686301d2a7
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-09-25 09:07:51 -0600

    need help

--- Arraylist Challenges/src/Array_Lists.java
+++ Arraylist Challenges/src/Array_Lists.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Array_Lists
 	{
@@ -5,31 +6,39 @@ public class Array_Lists
 		public static void main(String[] args)
 			{
 				practice();
-				Challenge_One();
-				Challenge_Two();
-				Challenge_Three();
+//				Challenge_One();
+//				Challenge_Two();
+//				Challenge_Three();
+				TwoDChallengeOne();
+				TwoDChallengeTwo();
+				TwoDChallengeThree();
+				
 				
 			}
+
+		
 
 		private static void practice()
 			{
 				//first number is the row
 				//second is the column
 				
-				int [] [] arrayScott = new int [3] [2];
+				ArrayList fiveDoubles = new ArrayList();
+				fiveDoubles.add("2.3");
+				fiveDoubles.add("4.1");
+				fiveDoubles.add("8.7");
+				fiveDoubles.add("2.2");
+				fiveDoubles.add("0.9");
 				
-				arrayScott [0] [0] = 5;
-				arrayScott [0] [1] = 10;
-				arrayScott [1] [0] = 15;
-				arrayScott [1] [1] = 20;
-				arrayScott [2] [0] = 25;
-				arrayScott [2] [1] = 46;
 				
-				System.out.println();
+				
+				
+				
 			}
 
 		private static void Challenge_One()
 			{
+				//ArrayList arrayOne = new ArrayList();
 				int arrayOne [] [] = new int [3] [4];
 				
 				arrayOne [0] [1] = 2;
@@ -51,19 +60,42 @@ public class Array_Lists
 
 		private static void Challenge_Two()
 			{
-				int [] [] mustangArray = new int [4] [3];
 				
-				for (int row = 0; row < mustangArray.length; row++)
-					{
-						for (int col = 0; col < mustangArray[0]length; col++)
-							{
-								mustangArray [row] [col]
-							}
-					}
 						
 			}
 
 		private static void Challenge_Three()
+			{
+				
+			}
+		private static void TwoDChallengeOne()
+			{
+				int [] [] arrayScott = new int [3] [2];
+				
+				arrayScott [0] [0] = 5;
+				arrayScott [0] [1] = 10;
+				arrayScott [1] [0] = 15;
+				arrayScott [1] [1] = 20;
+				arrayScott [2] [0] = 25;
+				arrayScott [2] [1] = 46;
+				
+				System.out.println();
+			}
+
+		private static void TwoDChallengeTwo()
+			{
+				int [] [] mustangArray = new int [4] [3];
+				
+				for (int row = 0; row < mustangArray.length; row++)
+					{
+//						for (int col = 0; col < mustangArray[0]length; col++)
+//							{
+//								mustangArray [row] [col]
+//							}
+					}
+			}
+
+		private static void TwoDChallengeThree()
 			{
 				
 			}

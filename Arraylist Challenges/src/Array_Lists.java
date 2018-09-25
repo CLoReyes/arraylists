fd452335c97b69407778e842094ae396b80fbb1d
carlos.reyes <carlos.reyes@MU-C1-18.mullen.pvt.k12.co.us>
2018-09-25 08:33:18 -0600

    fycjh

---
+++ Arraylist Challenges/src/Array_Lists.java
@@ -0,0 +1,71 @@
+
+public class Array_Lists
+	{
+
+		public static void main(String[] args)
+			{
+				practice();
+				Challenge_One();
+				Challenge_Two();
+				Challenge_Three();
+				
+			}
+
+		private static void practice()
+			{
+				//first number is the row
+				//second is the column
+				
+				int [] [] arrayScott = new int [3] [2];
+				
+				arrayScott [0] [0] = 5;
+				arrayScott [0] [1] = 10;
+				arrayScott [1] [0] = 15;
+				arrayScott [1] [1] = 20;
+				arrayScott [2] [0] = 25;
+				arrayScott [2] [1] = 46;
+				
+				System.out.println();
+			}
+
+		private static void Challenge_One()
+			{
+				int arrayOne [] [] = new int [3] [4];
+				
+				arrayOne [0] [1] = 2;
+				arrayOne [0] [2] = 4;
+				arrayOne [0] [3] = 6;
+				arrayOne [0] [4] = 8;
+				arrayOne [1] [0] = 3;
+				arrayOne [1] [1] = 6;
+				arrayOne [1] [2] = 9;
+				arrayOne [1] [3] = 12;
+				arrayOne [2] [0] = 4;
+				arrayOne [2] [2] = 8;
+				arrayOne [2] [3] = 12;
+				arrayOne [2] [4] = 16;
+				
+			//	System.out.println([1][2]);
+				
+			}
+
+		private static void Challenge_Two()
+			{
+				int [] [] mustangArray = new int [4] [3];
+				
+				for (int row = 0; row < mustangArray.length; row++)
+					{
+						for (int col = 0; col < mustangArray[0]length; col++)
+							{
+								mustangArray [row] [col]
+							}
+					}
+						
+			}
+
+		private static void Challenge_Three()
+			{
+				
+			}
+
+	}
